988deb3b2bffccc0562e79d5db616651f58a13ef
Samuel Gonzalez <xame2005@gmail.com>
2022-01-09 16:21:41 -0600

    [CREATED] Filters and Permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, filters
 from rest_framework import viewsets
-from profiles_api import serializers
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers, models, permissions
 
 
 class HelloApiView(APIView):
@@ -90,3 +92,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
