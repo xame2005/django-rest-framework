a38666b84d5a7922c2dd60daa8f1540943d4a52d
Samuel Gonzalez <xame2005@gmail.com>
2022-01-09 18:00:54 -0600

    [CREATED] Feed view and permissions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,8 +5,10 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewset)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
